221a8a28f812a8309b894e52983ead0096c52bfe
shogunsea <shogunsea@foxmail.com>
2014-03-02 18:40:43 -0500

    add candy solution.

---
+++ Candy.java
@@ -0,0 +1,39 @@
+// There are N children standing in a line. Each child is assigned a rating value.
+
+// You are giving candies to these children subjected to the following requirements:
+
+// Each child must have at least one candy.
+// Children with a higher rating get more candies than their neighbors.
+// What is the minimum candies you must give?
+
+public class Solution {
+    public int candy(int[] ratings) {
+        int len = ratings.length;
+        if(len==0){
+        	return 0;
+        }
+        int[] count = new int[len];
+
+        for(int i = 0; i<len; i++){
+        	count[i] = 1;
+        }
+
+        for(int i = 1; i<len; i++){
+        	if(ratings[i]>ratings[i-1]){
+        		count[i] = count[i-1]+1;
+        	}
+        }
+
+        for(int i = len-2; i>=0; i--){
+        	if(ratings[i]>ratings[i+1]&&count[i]<=count[i+1]){
+        		count[i] = count[i+1]+1;
+        	}
+        }
+
+        int sum = 0;
+        for(int i:count){
+        	sum+=i;
+        }
+        return sum;
+    }
+}
