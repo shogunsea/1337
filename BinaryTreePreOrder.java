715fd1336446d66b47af7c7e533975945e4e0e3e
shogunsea <shogunsea@foxmail.com>
2014-03-02 23:49:34 -0500

    add iterative solution for preorder traversal.

--- BinaryTreePreOrder.java
+++ BinaryTreePreOrder.java
@@ -1,19 +1,41 @@
 public class Solution {
+    // public ArrayList<Integer> preorderTraversal(TreeNode root) {
+    //     ArrayList<Integer> result = new ArrayList<Integer>();
+    //     if(root==null){
+    //     	return	result;
+    //     }
+    //     helper(result, root);
+    //     return result;
+    // }
+
+    // public void helper(ArrayList<Integer> result, TreeNode node){
+    // 	if(node==null){
+    // 		return;
+    // 	}
+    // 	result.add(node.val);
+    // 	helper(result, node.right);
+    // 	helper(result, node.left);
+    // }
+
+     // Iterative solution.
     public ArrayList<Integer> preorderTraversal(TreeNode root) {
         ArrayList<Integer> result = new ArrayList<Integer>();
         if(root==null){
-        	return	result;
+            return result;
         }
-        helper(result, root);
-        return result;
-    }
 
-    public void helper(ArrayList<Integer> result, TreeNode node){
-    	if(node==null){
-    		return;
-    	}
-    	result.add(node.val);
-    	helper(result, node.right);
-    	helper(result, node.left);
+        Stack<TreeNode> st = new Stack<TreeNode>();
+
+        st.push(root);
+
+        while(!st.isEmpty()){
+            TreeNode temp = st.pop();
+            if(temp!=null){
+                result.add(temp.val);
+                st.push(temp.right);
+                st.push(temp.left);
+            }
+        }
+        return result;
     }
 }
